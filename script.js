efe4f97517d7cea548ed7b48b93e0a337b73d0c2
Sean Demske <seandemske@gmail.com>
2020-07-28 13:47:43 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const videoElement = document.getElementById('video');
+const mediaSelect = document.getElementById('media-button');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
+
+mediaSelect.addEventListener('click', selectMediaStream);
